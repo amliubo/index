5b1a8ff6cbbdaf5de374a2a15d734986a184edd3
刘博 <liubo1@gg.com>
2024-08-16 16:43:36 +0800

    [初始] 初始化项目提交

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import BackgroundEffect from "./BackgroundEffect";
+
+function App() {
+  return (
+    <div className="App">
+      <BackgroundEffect />
+    </div>
+  );
+}
+
+export default App;
