9b6eb9954c251aea87a58703525bf9a22914689a
刘博 <liubo1@gg.com>
2025-02-17 17:02:40 +0800

    [修改] 布局相关

--- src/pages/About/AboutInfo.js
+++ src/pages/About/AboutInfo.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const AboutInfo = () => {
     return (
-        <div style={{ color: '#fff', padding: '20px', fontSize: '16px' }}>
-            <h2>DeepCraft——你的智能 AI 助手，助力高效创作与智能交互。</h2>
-            <p>一款基于 AI 的智能工具，旨在提升工作效率与创造力。</p>
-            <p>它融合了自然语言处理、智能交互和自动化功能，帮助用户轻松处理文本、数据分析、代码生成等任务。无论是开发者、内容创作者还是数据分析师，DeepCraft 都能提供强大的 AI 赋能，助力高效创作与决策。</p>
+        <div style={{ color: '#fff', fontSize: '16px', marginTop: '-27%' }}>
+            <h2>DeepCraft</h2>
+            <p>基于 DeepSeek-V3 智能工具，旨在提升工作效率与创造力。</p>
+            <p>融合自然语言处理、帮助你轻松处理文本、数据分析、代码生成等任务。无论你是开发者、内容创作者还是数据分析师，DeepCraft 都能提供强大的 AI 赋能，助力高效创作与决策。</p>
         </div>
     );
 };
