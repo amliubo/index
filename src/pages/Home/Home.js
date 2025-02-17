680453c2ad0f00ed57e0329b179e425bbc6ac53f
刘博 <liubo1@gg.com>
2025-02-17 15:16:25 +0800

    [修改] 项目结构

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Home.css";
 import MenuBar from "../../components/MenuBar";
 import Index from "../../components/Index";
-import PrivacyPolicy from "../../components/PrivacyPolicy";
-import AboutInfo from "../../components/AboutInfo";
+import PrivacyPolicy from "../Privacy/PrivacyPolicy";
+import AboutInfo from "../About/AboutInfo";
 import Copyright from "../../components/Copyright";
 function Home() {
     return (
@@ -12,7 +12,6 @@ function Home() {
             <div className="home-page">
                 <MenuBar />
                 <Routes>
-                    <Route path="/" element={<Index />} />
                     <Route path="/index" element={<Index />} />
                     <Route path="/about" element={<AboutInfo />} />
                     <Route path="/privacy" element={<PrivacyPolicy />} />
